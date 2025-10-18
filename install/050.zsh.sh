4255ad7fb55fa2d20b06e34d7154201995886059
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-10-18 11:47:12 -0300

    wip

--- install/050.zsh.sh
+++ install/050.zsh.sh
@@ -5,11 +5,9 @@ set -e
 chsh -s "$(which zsh)"
 
 # Install zsh zsh_plugins
-zsh_config_path="$HOME/.config/zsh/config"
-zsh_plugins_path="$HOME/.config/zsh/zsh_plugins"
+zsh_plugins_path="$HOME/.config/zsh/plugins"
 
 mkdir -p "$zsh_plugins_path"
-mkdir -p "$zsh_config_path"
 
 set +e
 git clone "https://github.com/zsh-users/zsh-autosuggestions.git" "$zsh_plugins_path/zsh-autosuggestions"
@@ -21,5 +19,5 @@ set -e
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
 # Create zsh symlinks
-ln -sfv "$WS_CONFIG_PATH/zsh/zsh/config" "$zsh_config_path"
+ln -sfv "$WS_CONFIG_PATH/zsh/config" "$HOME/.config/zsh"
 ln -sfv "$WS_CONFIG_PATH/zsh/.zshrc" "$HOME"

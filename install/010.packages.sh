0a06f157903a64a2648426209ede409384d86ff7
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-10-18 18:17:35 -0300

    "wip"

--- install/010.packages.sh
+++ install/010.packages.sh
@@ -14,8 +14,16 @@ sudo dnf install -y tar \
   fd-find \
   bat \
   cmake \
-  luarocks
+  lua5.1 \
+  lua5.1-devel \
+  luarocks \
+  nodejs
 
 # Lazygit
 sudo dnf copr enable -y atim/lazygit
 sudo dnf install -y lazygit
+
+# Rust and treesitter-cli
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+cargo install --locked tree-sitter-cli

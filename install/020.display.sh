f1a75e44361f1aa708acca226f3b03d53016f974
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-10-28 14:19:07 -0300

    "wip"

--- install/020.display.sh
+++ install/020.display.sh
@@ -19,5 +19,25 @@ sudo dnf install -y \
   git \
   wget \
   curl \
-  i3 \
-  lightdm
+  i3
+
+# Install LightDM without extra stuff
+sudo dnf install -y --setopt=install_weak_deps=False lightdm
+
+# Install feh
+# feh dependencies
+sudo dnf install -y \
+  imlib2 \
+  imlib2-devel \
+  libX11-devel \
+  libXinerama-devel \
+  libXext-devel \
+  libcurl-devel \
+  libXt-devel
+# Clone and build feh
+git clone https://github.com/derf/feh.git
+cd feh || exit
+make
+sudo make install
+cd .. || exit
+rm -rf feh

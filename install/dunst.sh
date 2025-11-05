92bd1391b483e106a0b9d18ac9c1c19fa9c369f4
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-11-04 21:26:02 -0300

    "wip"

--- install/dunst.sh
+++ install/dunst.sh
@@ -4,12 +4,12 @@ set -e
 
 # dunst dependencies
 sudo dnf install -y gcc make libX11-devel libXinerama-devel \
-  libXrandr-devel glib2-devel pango-devel \
+  libXrandr-devel glib2-devel pango-devel libXScrnSaver-devel \
   cairo-devel libnotify-devel dbus-devel
 
 # Build dunst
 cd "$WS_SOURCE_PATH" || exit
 git clone https://github.com/dunst-project/dunst.git
 cd dunst || exit
-make
-sudo make install
+make WAYLAND=0
+sudo make WAYLAND=0 install

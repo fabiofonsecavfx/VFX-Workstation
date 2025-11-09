cf4c322027a1018f1f27c6dbf5ae4237398497d5
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-11-09 17:00:31 -0300

    "wip"

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -147,7 +147,7 @@ layouts = [
     # layout.Stack(num_stacks=2),
     layout.Bsp(
         border_focus=colors["orange"],
-        border_normal=colors["bg1"]
+        border_normal=colors["bg"]
     ),
     # layout.Matrix(),
     # layout.MonadTall(),
@@ -214,7 +214,7 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
-follow_mouse_focus = False
+follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = False

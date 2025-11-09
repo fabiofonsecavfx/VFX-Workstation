2f02590502e41622d30ea083c29ddcb8b71daa83
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-11-09 15:50:48 -0300

    wip

--- install/system.sh
+++ install/system.sh
@@ -4,19 +4,19 @@ set -e
 
 # Install Groups
 sudo dnf groupinstall -y --with-optional \
-  "base-x" \
-  "Standard" \
-  "Hardware Support" \
-  "Input Methods" \
-  "Common NetworkManager Submodules" \
-  "Development Tools" \
-  "Fonts" \
-  "Multimedia"
+	"base-x" \
+	"Standard" \
+	"Hardware Support" \
+	"Input Methods" \
+	"Common NetworkManager Submodules" \
+	"Development Tools" \
+	"Fonts" \
+	"Multimedia"
 
 # Install Packages
 sudo dnf install -y libXScrnSaver libXpm libappindicator network-manager-applet \
-  pulseaudio-utils pavucontrol htop xclip thunar python3.11 python3.11-pip \
-  gnome-themes-extra papirus-icon-theme
+	pulseaudio-utils pavucontrol htop xclip thunar python3.11 python3.11-pip \
+	gnome-themes-extra papirus-icon-theme
 
 # Setup services and scripts
 sudo cp -v "$WS_SCRIPTS_PATH"/* /usr/local/bin
@@ -24,8 +24,8 @@ sudo cp -v "$WS_SERVICES_PATH"/* /etc/systemd/system
 
 services=("$WS_SERVICES_PATH"/*)
 for serv in "${services[@]}"; do
-  serv_name=$(basename "$serv")
-  sudo systemctl enable "$serv_name"
+	serv_name=$(basename "$serv")
+	sudo systemctl enable "$serv_name"
 done
 
 # Install Fonts
@@ -43,4 +43,4 @@ sudo mkdir -p /usr/local/share/icons
 sudo cp -fv "$WS_ASSETS_PATH"/icons/* /usr/local/share/icons/
 
 # Xresources
-ln -sfv "$WS_CONFIG_PATH/config/x11/.Xresources" "$HOME/.Xresources"
+ln -sfv "$WS_CONFIG_PATH/x11/.Xresources" "$HOME/.Xresources"

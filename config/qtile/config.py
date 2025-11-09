41a493f308dca6f3b25d4f1c7ee2261f7478e8f3
Fabio Fonseca <fabio.fonseca.cg@gmail.com>
2025-11-09 16:45:43 -0300

    "wip"

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -145,8 +145,8 @@ layouts = [
     #layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    layout.Matrix(),
+    layout.Bsp(),
+    # layout.Matrix(),
     # layout.MonadTall(),
     # layout.MonadWide(),
     # layout.RatioTile(),
